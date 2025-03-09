694352c980ba7e9bc35c05badec0db6f58de2d63
Kazi Mohammad Kaif <130026107+kazikaif@users.noreply.github.com>
2025-03-09 13:20:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 function highlight() {
-    //Write your code here
-
-
+    document.querySelectorAll('strong').forEach(el => el.style.color = 'rgb(0, 128, 0)');
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    document.querySelectorAll('strong').forEach(el => el.style.color = 'rgb(0, 0, 0)');
 }
